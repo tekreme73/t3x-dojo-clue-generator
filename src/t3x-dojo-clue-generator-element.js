c3744e75d037ae44057f6a75a60c4ba63fb03207
tekreme73 <tekreme73@gmail.com>
2019-02-02 05:12:33 +0100

    Improve 't3x-dojo-clue-generator-element' component with generation process tested from 't3x-dojo-clue-generator-tests.html' file

--- src/t3x-dojo-clue-generator-element.js
+++ src/t3x-dojo-clue-generator-element.js
@@ -31,13 +31,57 @@ export class T3XDojoClueGeneratorElement extends LitElement {
   }
 
   generate() {
-    let clues = Array(this.teams).fill(Array(this.challenges).fill({}));
+    let clues = [];
+    let _clues = [];
+
+    for(let team = 0; team < this.teams; team++) {
+      clues.push([]);
+      for(let challenge = 0; challenge < this.challenges; challenge++) {
+        _clues.push(team);
+      }
+    }
+
+    _clues = this.__shuffle(_clues);
+
+    for(let team = 0; team < this.teams; team++) {
+      for(let challenge = 0; challenge < this.challenges; challenge++) {
+        let index = 0;
+        let _team = _clues[index];
+        while(_team === team) {
+          _team = _clues[++index];
+        }
+
+        clues[team].push(this._createClue(_team, challenge));
+        _clues.splice( index, 1 ); // Remove the item from the array
+
+        if(index > _clues.length / 2) {
+          _clues = this.__shuffle(_clues);
+        }
+      }
+    }
 
     this.dispatchEvent(new CustomEvent('clues-generated', {
       detail: { clues, },
     }));
 
     return clues;
+  }
+
+  _createClue(team, challenge) {
+    return {
+      team,
+      challenge,
+    };
+  }
+  __shuffle(a) {
+    let j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
   }
 }
 
