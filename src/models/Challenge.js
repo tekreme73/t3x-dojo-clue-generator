ed4454702e519361b6d226515021e41f9bd23eea
tekreme73 <tekreme73@gmail.com>
2019-02-04 04:42:01 +0100

    Generate clues when the challenge is created

--- src/models/Challenge.js
+++ src/models/Challenge.js
@@ -7,7 +7,7 @@ export class Challenge {
     this.name = name;
     this.teams = teams;
 
-    this.clues = [];
+    this.clues = this.createClues(shuffle(this.teams)).sort((a, b) => a.getId() - b.getId());
   }
 
   getId() {
@@ -20,13 +20,10 @@ export class Challenge {
     return this.teams;
   }
   getClues() {
-    if(this.clues == null || this.clues.length <= this.teams.length) {
-      this.clues = this.createClues(shuffle(this.teams));
-    }
     return this.clues;
   }
 
-  createClues(teams = this.teams) {
+  createClues(teams) {
     let clues = [];
     for (let from = 0, to = 1; from < teams.length; from++ , to = ((to + 1) % teams.length)) {
       clues.push(new Clue(this.getId() * teams.length + from, teams[from], teams[to], this));
