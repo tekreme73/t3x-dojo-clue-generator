3d04ffa71b9deb39c6ed6621cedb3f9424b2ac8a
tekreme73 <tekreme73@gmail.com>
2019-02-06 00:39:52 +0100

    Create pieces in puzzle

--- src/models/Puzzle.js
+++ src/models/Puzzle.js
@@ -1,8 +1,11 @@
+import { Piece } from "./Piece";
 
 export class Puzzle {
-  constructor(id, name) {
+  constructor(id, name, piecesQuantity) {
     this.id = id;
     this.name = name;
+
+    this.pieces = this.createPieces(piecesQuantity);
   }
 
   getId() {
@@ -10,6 +13,18 @@ export class Puzzle {
   }
   getName() {
     return this.name;
+  }
+  getPieces() {
+    return this.pieces;
+  }
+
+  createPieces(quantity) {
+    const pieces = [];
+
+    for (let i = 0; i < quantity; i++) {
+      pieces.push(new Piece(this.getId() * quantity + i, `${this.getName()}-${i + 1}`, this));
+    }
+    return pieces;
   }
 
   toString() {
